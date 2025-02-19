032de288ed27bd8a6a1a3ff3833381c9bcb7f845
Theo Björkqvist <113356799+pengabutiken@users.noreply.github.com>
2025-02-19 11:04:57 +0100

    Laddar upp rätt filer
    Laddar upp de klara filerna så de finns på Git.

---
+++ LottoGenTheo/LottoGenTheo/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace LottoGenTheo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LottoRadPanel = new System.Windows.Forms.Panel();
+            this.LottoBox7 = new System.Windows.Forms.TextBox();
+            this.LottoBox6 = new System.Windows.Forms.TextBox();
+            this.LottoBox5 = new System.Windows.Forms.TextBox();
+            this.LottoBox4 = new System.Windows.Forms.TextBox();
+            this.LottoBox3 = new System.Windows.Forms.TextBox();
+            this.LottoBox2 = new System.Windows.Forms.TextBox();
+            this.LottoBox1 = new System.Windows.Forms.TextBox();
+            this.LottoRadLabel = new System.Windows.Forms.Label();
+            this.AntalDragningarPanel = new System.Windows.Forms.Panel();
+            this.StartaLottoButton = new System.Windows.Forms.Button();
+            this.AntalDragningarBox = new System.Windows.Forms.TextBox();
+            this.AntalDragningarLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.AntalRattBox3 = new System.Windows.Forms.TextBox();
+            this.AntalRattBox2 = new System.Windows.Forms.TextBox();
+            this.AntalRattBox1 = new System.Windows.Forms.TextBox();
+            this.RattLabel3 = new System.Windows.Forms.Label();
+            this.RattLabel2 = new System.Windows.Forms.Label();
+            this.RattLabel1 = new System.Windows.Forms.Label();
+            this.LottoRadPanel.SuspendLayout();
+            this.AntalDragningarPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LottoRadPanel
+            // 
+            this.LottoRadPanel.Controls.Add(this.LottoBox7);
+            this.LottoRadPanel.Controls.Add(this.LottoBox6);
+            this.LottoRadPanel.Controls.Add(this.LottoBox5);
+            this.LottoRadPanel.Controls.Add(this.LottoBox4);
+            this.LottoRadPanel.Controls.Add(this.LottoBox3);
+            this.LottoRadPanel.Controls.Add(this.LottoBox2);
+            this.LottoRadPanel.Controls.Add(this.LottoBox1);
+            this.LottoRadPanel.Controls.Add(this.LottoRadLabel);
+            this.LottoRadPanel.Location = new System.Drawing.Point(12, 12);
+            this.LottoRadPanel.Name = "LottoRadPanel";
+            this.LottoRadPanel.Size = new System.Drawing.Size(735, 63);
+            this.LottoRadPanel.TabIndex = 0;
+            // 
+            // LottoBox7
+            // 
+            this.LottoBox7.Location = new System.Drawing.Point(494, 22);
+            this.LottoBox7.Name = "LottoBox7";
+            this.LottoBox7.Size = new System.Drawing.Size(59, 22);
+            this.LottoBox7.TabIndex = 1;
+            // 
+            // LottoBox6
+            // 
+            this.LottoBox6.Location = new System.Drawing.Point(429, 22);
+            this.LottoBox6.Name = "LottoBox6";
+            this.LottoBox6.Size = new System.Drawing.Size(59, 22);
+            this.LottoBox6.TabIndex = 1;
+            // 
+            // LottoBox5
+            // 
+            this.LottoBox5.Location = new System.Drawing.Point(364, 22);
+            this.LottoBox5.Name = "LottoBox5";
+            this.LottoBox5.Size = new System.Drawing.Size(59, 22);
+            this.LottoBox5.TabIndex = 1;
+            // 
+            // LottoBox4
+            // 
+            this.LottoBox4.Location = new System.Drawing.Point(299, 22);
+            this.LottoBox4.Name = "LottoBox4";
+            this.LottoBox4.Size = new System.Drawing.Size(59, 22);
+            this.LottoBox4.TabIndex = 1;
+            // 
+            // LottoBox3
+            // 
+            this.LottoBox3.Location = new System.Drawing.Point(234, 22);
+            this.LottoBox3.Name = "LottoBox3";
+            this.LottoBox3.Size = new System.Drawing.Size(59, 22);
+            this.LottoBox3.TabIndex = 1;
+            // 
+            // LottoBox2
+            // 
+            this.LottoBox2.Location = new System.Drawing.Point(169, 22);
+            this.LottoBox2.Name = "LottoBox2";
+            this.LottoBox2.Size = new System.Drawing.Size(59, 22);
+            this.LottoBox2.TabIndex = 1;
+            // 
+            // LottoBox1
+            // 
+            this.LottoBox1.Location = new System.Drawing.Point(104, 22);
+            this.LottoBox1.Name = "LottoBox1";
+            this.LottoBox1.Size = new System.Drawing.Size(59, 22);
+            this.LottoBox1.TabIndex = 1;
+            // 
+            // LottoRadLabel
+            // 
+            this.LottoRadLabel.AutoSize = true;
+            this.LottoRadLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LottoRadLabel.Location = new System.Drawing.Point(7, 25);
+            this.LottoRadLabel.Name = "LottoRadLabel";
+            this.LottoRadLabel.Size = new System.Drawing.Size(91, 16);
+            this.LottoRadLabel.TabIndex = 0;
+            this.LottoRadLabel.Text = "Din lottorad:";
+            // 
+            // AntalDragningarPanel
+            // 
+            this.AntalDragningarPanel.Controls.Add(this.StartaLottoButton);
+            this.AntalDragningarPanel.Controls.Add(this.AntalDragningarBox);
+            this.AntalDragningarPanel.Controls.Add(this.AntalDragningarLabel);
+            this.AntalDragningarPanel.Location = new System.Drawing.Point(116, 81);
+            this.AntalDragningarPanel.Name = "AntalDragningarPanel";
+            this.AntalDragningarPanel.Size = new System.Drawing.Size(403, 56);
+            this.AntalDragningarPanel.TabIndex = 1;
+            // 
+            // StartaLottoButton
+            // 
+            this.StartaLottoButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.StartaLottoButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartaLottoButton.Location = new System.Drawing.Point(260, 3);
+            this.StartaLottoButton.Name = "StartaLottoButton";
+            this.StartaLottoButton.Size = new System.Drawing.Size(136, 50);
+            this.StartaLottoButton.TabIndex = 2;
+            this.StartaLottoButton.Text = "Starta Lotto";
+            this.StartaLottoButton.UseVisualStyleBackColor = false;
+            this.StartaLottoButton.Click += new System.EventHandler(this.StartaLottoButton_Click);
+            // 
+            // AntalDragningarBox
+            // 
+            this.AntalDragningarBox.Location = new System.Drawing.Point(135, 15);
+            this.AntalDragningarBox.Name = "AntalDragningarBox";
+            this.AntalDragningarBox.Size = new System.Drawing.Size(119, 22);
+            this.AntalDragningarBox.TabIndex = 1;
+            // 
+            // AntalDragningarLabel
+            // 
+            this.AntalDragningarLabel.AutoSize = true;
+            this.AntalDragningarLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AntalDragningarLabel.Location = new System.Drawing.Point(-1, 18);
+            this.AntalDragningarLabel.Name = "AntalDragningarLabel";
+            this.AntalDragningarLabel.Size = new System.Drawing.Size(125, 16);
+            this.AntalDragningarLabel.TabIndex = 0;
+            this.AntalDragningarLabel.Text = "Antal dragningar:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.AntalRattBox3);
+            this.panel1.Controls.Add(this.AntalRattBox2);
+            this.panel1.Controls.Add(this.AntalRattBox1);
+            this.panel1.Controls.Add(this.RattLabel3);
+            this.panel1.Controls.Add(this.RattLabel2);
+            this.panel1.Controls.Add(this.RattLabel1);
+            this.panel1.Location = new System.Drawing.Point(143, 159);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(340, 29);
+            this.panel1.TabIndex = 2;
+            // 
+            // AntalRattBox3
+            // 
+            this.AntalRattBox3.Location = new System.Drawing.Point(277, 4);
+            this.AntalRattBox3.Name = "AntalRattBox3";
+            this.AntalRattBox3.ReadOnly = true;
+            this.AntalRattBox3.Size = new System.Drawing.Size(60, 22);
+            this.AntalRattBox3.TabIndex = 3;
+            // 
+            // AntalRattBox2
+            // 
+            this.AntalRattBox2.Location = new System.Drawing.Point(161, 4);
+            this.AntalRattBox2.Name = "AntalRattBox2";
+            this.AntalRattBox2.ReadOnly = true;
+            this.AntalRattBox2.Size = new System.Drawing.Size(59, 22);
+            this.AntalRattBox2.TabIndex = 2;
+            // 
+            // AntalRattBox1
+            // 
+            this.AntalRattBox1.Location = new System.Drawing.Point(46, 4);
+            this.AntalRattBox1.Name = "AntalRattBox1";
+            this.AntalRattBox1.ReadOnly = true;
+            this.AntalRattBox1.Size = new System.Drawing.Size(59, 22);
+            this.AntalRattBox1.TabIndex = 1;
+            // 
+            // RattLabel3
+            // 
+            this.RattLabel3.AutoSize = true;
+            this.RattLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RattLabel3.Location = new System.Drawing.Point(226, 7);
+            this.RattLabel3.Name = "RattLabel3";
+            this.RattLabel3.Size = new System.Drawing.Size(45, 16);
+            this.RattLabel3.TabIndex = 0;
+            this.RattLabel3.Text = "7 rätt:";
+            // 
+            // RattLabel2
+            // 
+            this.RattLabel2.AutoSize = true;
+            this.RattLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RattLabel2.Location = new System.Drawing.Point(111, 7);
+            this.RattLabel2.Name = "RattLabel2";
+            this.RattLabel2.Size = new System.Drawing.Size(45, 16);
+            this.RattLabel2.TabIndex = 0;
+            this.RattLabel2.Text = "6 rätt:";
+            // 
+            // RattLabel1
+            // 
+            this.RattLabel1.AutoSize = true;
+            this.RattLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RattLabel1.Location = new System.Drawing.Point(-5, 7);
+            this.RattLabel1.Name = "RattLabel1";
+            this.RattLabel1.Size = new System.Drawing.Size(45, 16);
+            this.RattLabel1.TabIndex = 0;
+            this.RattLabel1.Text = "5 rätt:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 200);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.AntalDragningarPanel);
+            this.Controls.Add(this.LottoRadPanel);
+            this.MaximumSize = new System.Drawing.Size(600, 247);
+            this.MinimumSize = new System.Drawing.Size(600, 247);
+            this.Name = "Form1";
+            this.Text = "Theos Lotto Generator";
+            this.LottoRadPanel.ResumeLayout(false);
+            this.LottoRadPanel.PerformLayout();
+            this.AntalDragningarPanel.ResumeLayout(false);
+            this.AntalDragningarPanel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel LottoRadPanel;
+        private System.Windows.Forms.TextBox LottoBox7;
+        private System.Windows.Forms.TextBox LottoBox6;
+        private System.Windows.Forms.TextBox LottoBox5;
+        private System.Windows.Forms.TextBox LottoBox4;
+        private System.Windows.Forms.TextBox LottoBox3;
+        private System.Windows.Forms.TextBox LottoBox2;
+        private System.Windows.Forms.TextBox LottoBox1;
+        private System.Windows.Forms.Label LottoRadLabel;
+        private System.Windows.Forms.Panel AntalDragningarPanel;
+        private System.Windows.Forms.Label AntalDragningarLabel;
+        private System.Windows.Forms.Button StartaLottoButton;
+        private System.Windows.Forms.TextBox AntalDragningarBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label RattLabel1;
+        private System.Windows.Forms.TextBox AntalRattBox2;
+        private System.Windows.Forms.TextBox AntalRattBox1;
+        private System.Windows.Forms.Label RattLabel3;
+        private System.Windows.Forms.Label RattLabel2;
+        private System.Windows.Forms.TextBox AntalRattBox3;
+    }
+}
+
---
+++ LottoGenTheo/LottoGenTheo/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LottoGenTheo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartaLottoButton_Click(object sender, EventArgs e)
+        {
+            
+
+            try
+            {
+                int[] LottoRad = {
+                    TestaNummer(LottoBox1.Text),
+                    TestaNummer(LottoBox2.Text),
+                    TestaNummer(LottoBox3.Text),
+                    TestaNummer(LottoBox4.Text),
+                    TestaNummer(LottoBox5.Text),
+                    TestaNummer(LottoBox6.Text),
+                    TestaNummer(LottoBox7.Text)
+                };
+
+                if (LottoRad.Distinct().Count() != 7)
+                {
+                    throw new Exception("Varje nummer måste vara unikt.");
+                }
+
+
+
+                int antalDragningar = TestaAntalDragningar(AntalDragningarBox.Text);
+                SimuleraLotto(LottoRad, antalDragningar);
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
+        private int TestaNummer(string input)
+        {
+            if (int.TryParse(input, out int nummer) && nummer >= 1 && nummer <= 35)
+            {
+
+                return nummer;
+            }
+            else
+            {
+                throw new Exception("Ange siffta mellan 1 och 35");
+            }
+
+        }
+        private int TestaAntalDragningar(string input)
+        {
+            if(int.TryParse(input,out int antal) && antal>=1 && antal <= 999999)
+            {
+                return antal;
+            }
+            else
+            {
+                throw new Exception("Ange ett antal dragningar, MÅSTE vara mellan 1-999999");
+            }
+        }
+        private void SimuleraLotto(int[] spelarRad,int antalDragningar)
+        {
+            Random rand = new Random();
+            int femRatt = 0, sexRatt = 0, sjuRatt = 0;
+            for (int i = 0; i < antalDragningar; i++)
+            {
+                int[] dragning = GenereraLottoRad(rand);
+                int antalRatt = spelarRad.Intersect(dragning).Count();
+
+                if (antalRatt == 5)
+                {
+                    femRatt++;
+                }
+                else if (antalRatt == 6)
+                {
+                    sexRatt++;
+                }
+                else if (antalRatt == 7)
+                {
+                    sjuRatt++;
+
+                }
+            }
+            AntalRattBox1.Text = femRatt.ToString();
+            AntalRattBox2.Text=sexRatt.ToString();
+            AntalRattBox3.Text=sjuRatt.ToString() ;
+
+
+        }
+        private int[] GenereraLottoRad(Random rnd)
+        {
+            return Enumerable.Range(1, 35).OrderBy(x => rnd.Next()).Take(7).ToArray();
+        }
+    }
+}
